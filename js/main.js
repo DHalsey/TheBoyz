3bd7f290ff4043b99969e7eef55c67164c72761f
DHalsey <dhalsey@ucsc.edu>
2017-05-06 17:14:27 -0700

    For Salvador

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 //main.js
 var game;
-
+var map;
+var layerCollision;
+var player;
 //global groups
 var bullets;
 
@@ -21,7 +23,7 @@ Preloader.prototype = {
 		game.load.image('mapImage','MapTiles.png'); //tilemap images
 		game.load.image('rifleSprite', 'weapon_rifle.png');
 		game.load.image('collisionImage','Collision.png'); //tilemap images
-
+		game.physics.startSystem(Phaser.Physics.ARCADE);
 	},
 	create: function(){
 		game.state.start('Play');
@@ -31,24 +33,37 @@ Preloader.prototype = {
 var Play = function(game) {var player};
 Play.prototype = {
 	preload: function(){
-
+		
 	},
 	create: function(){
-		var map = game.add.tilemap('maptile');
-        map.addTilesetImage('Map','mapImage');
-        //game.physics.enable(mapImage, Phaser.Physics.ARCADE);
+		map = game.add.tilemap('maptile');
+
+		map.addTilesetImage('Map','mapImage');
         layerMain = map.createLayer('worldMain'); //main world layer
+
+
         map.addTilesetImage('Collision','collisionImage');
+        map.setCollisionBetween(1,20); //not totally sure what this does
+        map.setCollision(3); //not totally sure what this does
+
         layerCollision = map.createLayer('CollisionBounds'); //main world layer
+        layerCollision.alpha = 150;
+
+        //game.add.existing(layerCollision);
+        //game.physics.arcade.enable(layerCollision);
+        
+
+
+
         player = new Player(game, 200, 200, 'atlas', 'player0001', 10);
         enemy = new Enemy1(game, 400, 200, 'atlas', 'player0002', 5, player);
         var rifle = new Weapon(game, game.world.width/2, game.world.height/2, 'rifleSprite', 'rifle', 
         	100, player);
         //create groups
         bullets = game.add.physicsGroup();
-
+        
 	},
 	update: function(){
-        
+        game.physics.arcade.collide(player, layerCollision);
 	}
 };
