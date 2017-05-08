9dddcb5d538d932474e7e3996f1fe18b639c44c0
dhalsey <dhalsey@ucsc.edu>
2017-05-08 13:34:19 -0700

    removed redundant line of code

--- js/knockback.js
+++ js/knockback.js
@@ -1,5 +1,4 @@
 function knockback(object,force,angle){
-	player.body.drag.x = 1000; //TESTCODE
 	object.body.velocity.x -= Math.cos(angle) * force;
 	object.body.velocity.y -= Math.sin(angle) * force;
 }
