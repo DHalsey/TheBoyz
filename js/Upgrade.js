da57b65ff24f483ad66642d1957a1ce6453cc78a
Austin Kellar <Austin Kellar>
2017-05-22 10:54:55 -0700

    upgrade menu is dynamic

--- js/Upgrade.js
+++ js/Upgrade.js
@@ -1,5 +1,10 @@
+//create upgrades array
+var possibleUpgrades = new Array('hp', 'pistol', 'rifle', 'shotgun', 'dash', 'skip');
+var possibleY = new Array(175, 250, 325, 400, 475, 550);
+
+
 //Upgrade.js
-var Upgrade = function(game) {};
+var Upgrade = function(game) {var buttonString, button, buttonFunction;};
 Upgrade.prototype = {
 	preload: function() {
 
@@ -9,44 +14,33 @@ Upgrade.prototype = {
 		game.add.image(0, 0, 'loadBackground');
 		game.add.text(500 - 50, 80, 'Choose an Upgrade', {fontSize: '32px', fill: "#000000"});
 
-		//upgrade health
-		if(!healthUpgraded) {
-			var healthButton = game.add.button(500, 200, 'genericButton', upgradeHealth, this);
-			healthButton.inputEnabled = true;
-			healthButton.useHandCursor = false;
-			game.add.text(510, 225, '+ Max HP', {fontSize: '12px', fill: '#000000'});
-		}
+		for(var i=0; i<possibleUpgrades.length; i++) {
 
-		//upgrade pistol
-		if(!pistolUpgraded) {
-			var pistolButton = game.add.button(500, 300, 'genericButton', upgradePistol, this);
-			pistolButton.inputEnabled = true;
-			pistolButton.useHandCursor = false;
-			game.add.text(510, 325, 'Pistol 2 Round Burst', {fontSize: '12px', fill: '#000000'});
-		}
+			if(possibleUpgrades[i] === 'hp') {
+				buttonString = '+ Max HP';
+				buttonFunction = upgradeHealth;
+			} else if(possibleUpgrades[i] === 'pistol') {
+				buttonString = 'Pistol 2 Round Burst';
+				buttonFunction = upgradePistol;
+			} else if(possibleUpgrades[i] === 'rifle') {
+				buttonString = '+ Rifle Fire Rate';
+				buttonFunction = upgradeRifle;
+			} else if(possibleUpgrades[i] === 'shotgun') {
+				buttonString = '+ Shotgun Damage';
+				buttonFunction = upgradeShotgun;
+			} else if(possibleUpgrades[i] === 'dash') {
+				buttonString = 'Dash Ability';
+				buttonFunction = enableDash;
+			}
+			else {
+				buttonString = 'No Upgrade';
+				buttonFunction = skipUpgrade;
+			}
 
-		//upgrade rifle
-		if(!rifleUpgraded) {
-			var rifleButton = game.add.button(500, 400, 'genericButton', upgradeRifle, this);
-			rifleButton.inputEnabled = true;
-			rifleButton.useHandCursor = false;
-			game.add.text(510, 425, '+ Rifle Fire Rate', {fontSize: '12px', fill: '#000000'});
-		}
-
-		//upgrade shotgun
-		if(!shotgunUpgraded) {
-			var shotgunButton = game.add.button(500, 500, 'genericButton', upgradeShotgun, this);
-			shotgunButton.inputEnabled = true;
-			shotgunButton.useHandCursor = false;
-			game.add.text(510, 525, '+ Shotgun Damage', {fontSize: '12px', fill: '#000000'});
-		}
-
-		//enable dash ability
-		if(!dashEnabled) {
-			var dashButton = game.add.button(500, 600, 'genericButton', enableDash, this);
-			dashButton.inputEnabled = true;
-			dashButton.useHandCursor = false;
-			game.add.text(510, 625, 'Acquire Dash Ability', {fontSize: '12px', fill: '#000000'});
+			button = game.add.button(500, possibleY[i], 'genericButton', buttonFunction, this);
+			button.inputEnabled = true;
+			button.useHandCursor = false;
+			game.add.text(510, possibleY[i] + 25, buttonString, {fontSize: '12px', fill: '#000000'});
 		}
 	},
 	update: function() {
@@ -56,31 +50,40 @@ Upgrade.prototype = {
 
 function upgradeHealth() {
 	statChanger.changeHealth();
-	healthUpgraded = true;
+	var index = possibleUpgrades.indexOf('hp');
+	if(index > -1) possibleUpgrades.splice(index, 1);
 	game.state.start(nextLevel);
 }
 
 function upgradePistol() {
 	statChanger.changePistol();
-	pistolUpgraded = true;
+	var index = possibleUpgrades.indexOf('pistol');
+	if(index > -1) possibleUpgrades.splice(index, 1);
 	game.state.start(nextLevel);
 }
 
 function upgradeRifle() {
 	statChanger.changeRifle();
-	rifleUpgraded = true;
+	var index = possibleUpgrades.indexOf('rifle');
+	if(index > -1) possibleUpgrades.splice(index, 1);
 	game.state.start(nextLevel);
 }
 
 function upgradeShotgun() {
 	statChanger.changeShotgun();
-	shotgunUpgraded = true;
+	var index = possibleUpgrades.indexOf('shotgun');
+	if(index > -1) possibleUpgrades.splice(index, 1);
 	game.state.start(nextLevel);
 }
 
 
 function enableDash() {
 	statChanger.changeDash();
-	dashEnabled = true;
+	var index = possibleUpgrades.indexOf('dash');
+	if(index > -1) possibleUpgrades.splice(index, 1);
+	game.state.start(nextLevel);
+}
+
+function skipUpgrade() {
 	game.state.start(nextLevel);
 }
